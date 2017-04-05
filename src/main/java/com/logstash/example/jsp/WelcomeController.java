9a3ea1c0a2882f1a57b9b9724871339ff0687e2c
RameshRajendran <ramesh.rajendran@mastek.com>
2017-04-05 15:45:29 +0100

    new import

---
+++ src/main/java/com/logstash/example/jsp/WelcomeController.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.logstash.example.jsp;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+@Controller
+public class WelcomeController {
+	public static final Logger LOGGER = getLogger(WelcomeController.class);
+	@Value("${application.message:Hello World}")
+	private String message = "Hello World";
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		LOGGER.info("Welcome controller loading = " + model);
+		model.put("time", new Date());
+		model.put("message", this.message);
+		return "welcome";
+	}
+
+}
